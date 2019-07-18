a4a86524fb41abde7cf9978dbe7540253b916f30
Gustavohts <35411938+Gustavohts@users.noreply.github.com>
2019-07-18 12:29:32 -0300

    Update Detection.py

--- Detection.py
+++ Detection.py
@@ -1,16 +1,16 @@
 import cv2
 
-classificador = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+classifier = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
 
-imagem = cv2.imread('photos/pessoas3.jpg')
-imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+image = cv2.imread('photos/pessoas3.jpg')
+imageGray = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
-facesDetectadas = classificador.detectMultiScale(imagemCinza, scaleFactor=1.1)
-print(len(facesDetectadas))
-print(facesDetectadas)
+facesDetected = classifier.detectMultiScale(imageGray, scaleFactor=1.1)
+print(len(facesDetected))
+print(facesDetected)
 
-for(x, y, l, a) in facesDetectadas:
-    cv2.rectangle(imagem, (x,y), (x + l, y + a), (255, 0, 255), 2)
+for(x, y, l, a) in facesDetected:
+    cv2.rectangle(image, (x,y), (x + l, y + a), (255, 0, 255), 2)
 
-cv2.imshow("Faces encontradas", imagem)
+cv2.imshow("Found", image)
 cv2.waitKey()
